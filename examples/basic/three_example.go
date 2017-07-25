cebf098a78ff07160b50ee47ad18eabf8848ceaa
Luis Ramos <lngramos@me.com>
2017-07-25 17:54:59 +0100

    Cleanup

--- examples/basic/three_example.go
+++ examples/basic/three_example.go
@@ -12,24 +12,6 @@ var (
 	mesh     three.Mesh
 )
 
-// var camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
-// camera.position.z = 400;
-
-// var scene = new THREE.Scene();
-
-// var geometry = new THREE.BoxBufferGeometry( 200, 200, 200 );
-// var material = new THREE.MeshBasicMaterial( { color: "red" } );
-
-// var mesh = new THREE.Mesh( geometry, material );
-// scene.add( mesh );
-
-// var renderer = new THREE.WebGLRenderer();
-// renderer.setPixelRatio( window.devicePixelRatio );
-// renderer.setSize( window.innerWidth, window.innerHeight );
-// document.body.appendChild( renderer.domElement );
-
-// renderer.render( scene, camera );
-
 func main() {
 	document := js.Global.Get("document")
 	windowWidth := js.Global.Get("innerWidth").Float()
@@ -40,7 +22,6 @@ func main() {
 	camera.Position.Set(0, 0, 400)
 
 	scene = three.NewScene()
-	// camera.Position.Z = 5
 
 	renderer = three.NewWebGLRenderer()
 	renderer.SetPixelRatio(devicePixelRatio)
